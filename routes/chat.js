988d42846748f394f1af7b0c28d21fc34cccdd30
JANGSEYEONG <jang.se.yeong000@gmail.com>
2024-01-06 01:27:40 +0900

    채팅방 삭제 기능 추가 / 상세페이지 ui 수정

--- routes/chat.js
+++ routes/chat.js
@@ -25,16 +25,17 @@ router.get('/request', async (req, res) => {
 
         // 1. db에 해당하는 채팅방이 있는지 확인한다.
         // 채팅방 생성 기준 : 멤버에 나와 상대가 있는 채팅방이 있는지 조회
+        let chatRoomId = null;
         const findQuery = {
             $or:[{member:[new ObjectId(partner._id), user._id]},{member:[user._id,new ObjectId(partner._id)]}]
         };
         const result = await db.collection('chatRoom').find(findQuery).toArray();
-        let chatRoomId = null;
+        
         console.log(result);
         if(result.length === 0){
             // 1-1. 채팅방이 없을 경우 채팅방 생성 후 채팅 상세 페이지로 연결한다
             console.log('채팅방 없음')
-            let result2 = await db.collection('chatRoom').insertOne({
+            const result2 = await db.collection('chatRoom').insertOne({
                 member: [user._id, new ObjectId(partner._id)],
                 date: moment().format('YYYY-MM-DD HH:mm:ss'),
             });
@@ -64,7 +65,7 @@ router.get('/list', async (req, res) => {
         const user = req.user;
         
         // 1. 내가 포함된 채팅방 리스트 가져오기
-        let result = await db.collection('chatRoom').find({ member: user._id }).toArray(); // member는 배열이지만 아래와 같이 쿼리를 짜도 잘 가져옴
+        const result = await db.collection('chatRoom').find({ member: user._id }).toArray(); // member는 배열이지만 아래와 같이 쿼리를 짜도 잘 가져옴
         // console.log(result);
 
         // 2. 상대방 id만 뽑아오기
@@ -75,7 +76,7 @@ router.get('/list', async (req, res) => {
         });
 
         // 3. DB에서 상대 nickname 가져오기, patrnerId 배열 순서대로 가져와야해서 id도 함께 조회한다
-        let partnerNick = await db.collection('user').find({_id : {$in : partnerId}}, {projection : {usernick : 1}}).toArray();
+        const partnerNick = await db.collection('user').find({_id : {$in : partnerId}}, {projection : {usernick : 1}}).toArray();
 
         // 3-1.id와 nickname 순서 맞춰주며 chatList에 상대 nickname 합치기
         partnerId.forEach((x, i)=>{
@@ -97,17 +98,22 @@ router.get('/list', async (req, res) => {
 router.get('/detail/:id', async (req, res) => {
     try {
         const user = req.user;
-        const chatId = req.params.id;
+        const chatRoomId = req.params.id;
 
-        let result = await db.collection('chatRoom').findOne({
-            _id: new ObjectId(chatId),
+        // 0. 현재 채팅방 정보
+        const result = await db.collection('chatRoom').findOne({
+            _id: new ObjectId(chatRoomId),
         });
+
+        // 1. 파라미터로 전달받은 chatRoomId를 parentRoomId로 가지고 있는 document를 가져온다.
+        const chatData = db.collection('chatMessage').find({
+            parentChatRoomId  : new ObjectId(chatRoomId)
+        }).toArray();
 
         let isMember = result.member.some(x => x.toString() === user._id.toString());
 
-
         if (isMember) {
-            res.render('chatDetail.ejs', { user: user, result: result });
+            res.render('chatDetail.ejs', { user: user, result: result, chatData : [] });
         } else {
             res.send('너의 채팅방이 아니란다');
         }
@@ -116,5 +122,37 @@ router.get('/detail/:id', async (req, res) => {
         return res.send('error: ' + e);
     }
 });
+
+// 채팅방 삭제
+router.delete('/roomDelete', async (req, res, next)=>{
+    try{
+        const user = req.user;
+        const chatRoomId = req.query.id;
+        //console.log(chatRoomId)
+        
+        // 1. 채팅방 삭제 (chatRoomId, member에 로그인한 유저가 포함되어 있는지 두가지로 체크)
+        const roomQuery = {_id : new ObjectId(chatRoomId), member : new ObjectId(user._id)};
+        const roomResult = await db.collection('chatRoom').deleteOne(roomQuery);
+        console.log(roomResult.deletedCount);
+
+        // 2. 채팅방을 부모로 가진 채팅 메세지 삭제
+        const msgQuery = {parentChatRoomId : new ObjectId(chatRoomId)};
+        const msgResult = await db.collection('chatMessage').deleteMany(msgQuery);
+        console.log(msgResult.deletedCount);
+        
+
+        if(roomResult.deletedCount < 1){
+            return res.status(400).send('잘못된 요청입니다.');
+        }else{
+            return res.status(200).send('삭제 완료');
+        }
+
+    }catch(e){
+        Log.Write('chat/roomDelete[delete]', e, true);
+        return res.send('error: ' + e);
+    }
+    
+});
+
 
 module.exports = router;
